d9109f1b0173aaefdd7291dba07e186e4faf6e37
Andrew Frahn <emmertex@gmail.com>
2018-08-03 10:36:17 +1000

    Little Endian Support, thanks @rickbias

--- selfdrive/can/packer.cc
+++ selfdrive/can/packer.cc
@@ -1,32 +1,35 @@
 #include <cassert>
-
 #include <string>
 #include <vector>
 #include <utility>
 #include <algorithm>
 #include <map>
 #include <cmath>
-
 #include "common.h"
-
 #define WARN printf
-
 namespace {
-
+  // this is the same as read_u64_le, but uses uint64_t as in/out
+  uint64_t ReverseBytes(uint64_t val) {
+      val = ((val << 8) & 0xFF00FF00FF00FF00ULL) | ((val >> 8) & 0x00FF00FF00FF00FFULL);
+      val = ((val << 16) & 0xFFFF0000FFFF0000ULL) | ((val >> 16) & 0x0000FFFF0000FFFFULL);
+      return (val << 32) | (val >> 32);
+  }
   uint64_t set_value(uint64_t ret, Signal sig, int64_t ival){
-    uint64_t mask = ((1ULL << sig.b2)-1) << sig.bo;
-    uint64_t dat = (ival & ((1ULL << sig.b2)-1)) << sig.bo;
+    int shift = sig.is_little_endian? sig.b1 : sig.bo;
+    uint64_t mask = ((1ULL << sig.b2)-1) << shift;
+    uint64_t dat = (ival & ((1ULL << sig.b2)-1)) << shift;
+    if (sig.is_little_endian) {
+      dat = ReverseBytes(dat);
+    }
     ret &= ~mask;
     ret |= dat;
     return ret;
   }
-
   class CANPacker {
   public:
     CANPacker(const std::string& dbc_name) {
       dbc = dbc_lookup(dbc_name);
       assert(dbc);
-
       for (int i=0; i<dbc->num_msgs; i++) {
         const Msg* msg = &dbc->msgs[i];
         message_lookup[msg->address] = *msg;
@@ -36,28 +39,23 @@ namespace {
         }
       }
     }
-
     uint64_t pack(uint32_t address, const std::vector<SignalPackValue> &signals, int counter) {
       uint64_t ret = 0;
       for (const auto& sigval : signals) {
         std::string name = std::string(sigval.name);
         double value = sigval.value;
-
         auto sig_it = signal_lookup.find(std::make_pair(address, name));
         if (sig_it == signal_lookup.end()) {
           WARN("undefined signal %s - %d\n", name.c_str(), address);
           continue;
         }
         auto sig = sig_it->second;
-
         int64_t ival = (int64_t)(round((value - sig.offset) / sig.factor));
         if (ival < 0) {
           ival = (1ULL << sig.b2) + ival;
         }
-
         ret = set_value(ret, sig, ival);
       }
-
       if (counter >= 0){
         auto sig_it = signal_lookup.find(std::make_pair(address, "COUNTER"));
         if (sig_it == signal_lookup.end()) {
@@ -65,14 +63,11 @@ namespace {
           return ret;
         }
         auto sig = sig_it->second;
-
         if (sig.type != SignalType::HONDA_COUNTER){
           WARN("COUNTER signal type not valid\n");
         }
-
         ret = set_value(ret, sig, counter);
       }
-
       auto sig_it = signal_lookup.find(std::make_pair(address, "CHECKSUM"));
       if (sig_it != signal_lookup.end()) {
         auto sig = sig_it->second;
@@ -87,30 +82,21 @@ namespace {
           WARN("CHECKSUM signal type not valid\n");
         }
       }
-
       return ret;
     }
-
-
-
   private:
     const DBC *dbc = NULL;
     std::map<std::pair<uint32_t, std::string>, Signal> signal_lookup;
     std::map<uint32_t, Msg> message_lookup;
   };
-
 }
-
 extern "C" {
   void* canpack_init(const char* dbc_name) {
     CANPacker *ret = new CANPacker(std::string(dbc_name));
     return (void*)ret;
   }
-
   uint64_t canpack_pack(void* inst, uint32_t address, size_t num_vals, const SignalPackValue *vals, int counter, bool checksum) {
     CANPacker *cp = (CANPacker*)inst;
-
     return cp->pack(address, std::vector<SignalPackValue>(vals, vals+num_vals), counter);
   }
-
 }
